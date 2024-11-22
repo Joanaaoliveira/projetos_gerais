a6e9ed7146f1be5acf6587741ce82193e82cc745
Joana Oliveira <oliveiraapjoana@gmail.com>
2024-11-22 16:45:46 -0300

    incluindo pratica sql d_22112024

---
+++ SQL/hospital/hospital_pratica03.sql
@@ -0,0 +1,14 @@
+/*  Show first name, last name, and the full province name of each patient.
+Example: 'Ontario' instead of 'ON' */
+SELECT 
+  p.first_name,
+  p.last_name,
+  pn.province_name
+FROM 
+  patients AS p
+INNER JOIN 
+  province_names AS pn 
+  ON p.province_id = pn.province_id;
+
+
+
