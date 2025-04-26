9e72a94bd42f2f44a043fb388105b3abf8b4f139
Joana Oliveira <oliveiraapjoana@gmail.com>
2025-04-26 09:37:31 -0300

    incluindo modificacoes d_26042025

--- SQL/hospital/hospital_pratica03.sql
+++ SQL/hospital/hospital_pratica03.sql
@@ -10,5 +10,16 @@ INNER JOIN
   province_names AS pn 
   ON p.province_id = pn.province_id;
 
+  /* Show how many patients have a birth_date with 2010 as the birth year. */
+
+SELECT 
+  COUNT(patient_id)
+FROM 
+  patients
+WHERE 
+  birth_date LIKE '2010%';
+
+  
+
 
 
